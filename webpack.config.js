6a07ebb757086dbbae46769d6b04ceec8d58ff8f
Eldar <p.eldar2005@gmail.com>
2023-10-17 16:16:51 +0600

    init react-app

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  entry: "./src/index.js", // Ваш главный файл JavaScript
-  output: {
-    filename: "bundle.js", // Имя выходного файла
-    path: path.resolve(__dirname, "dist"), // Папка, в которой будет сохранен собранный файл
-  },
-};
